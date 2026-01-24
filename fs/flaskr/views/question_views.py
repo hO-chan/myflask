f7b08a2cc737d00e8325abf6fe9802e5e1e84a96
hO-chan <hwang151776@gmail.com>
2026-01-25 00:21:45 +0900

    3-02 Paging with pagination

--- fs/flaskr/views/question_views.py
+++ fs/flaskr/views/question_views.py
@@ -11,10 +11,10 @@ from flaskr import db
 bp = Blueprint('question', __name__, url_prefix='/question')
 
 
-@bp.route('/list/')
-def _list():
-    question_list = Question.query.order_by(Question.create_date.desc())
-    return render_template('question/question_list.html', question_list=question_list)
+# @bp.route('/list/')
+# def _list():
+#     question_list = Question.query.order_by(Question.create_date.desc())
+#     return render_template('question/question_list.html', question_list=question_list)
 
 
 @bp.route('/detail/<int:question_id>/')
@@ -37,3 +37,15 @@ def create():
         return redirect(url_for('main.index'))
 
     return render_template('question/question_form.html',form=form)
+
+@bp.route('/list/')
+def _list():
+    #get 방식으로 욫어한 url 에서 page 값을 가져올 때 사용
+    page = request.args.get('page',type= int, default=1) #페이지
+
+    question_list = Question.query.order_by(Question.create_date.desc())
+    #조회한 데이터 q_list에 paginate 함수로 페이징을 적용
+    ## (현재 조회할 페이지 번호, 부터 ㅁ개씩 보여주기)
+    ## Pagination 객체가 return 됨
+    question_list =question_list.paginate(page=page,per_page=10)
+    return render_template('question/question_list.html',question_list=question_list)
